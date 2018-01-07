f2c14f25237c78bdd3302be68170abe5c51a7412
Kamil Borkowski <kamil.borkowski.02@gmail.com>
2018-01-07 23:55:23 +0100

    Check if user is logged-in on App's start

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,25 @@
 </template>
 
 <script>
+  import { mapActions } from 'vuex'
+  import ls from 'local-storage'
+  import { STORAGE_AUTH_TOKEN } from 'src/constants'
+
   export default {
     name: 'App',
+    methods: {
+      ...mapActions({
+        login: 'auth/login',
+      }),
+    },
+    async mounted() {
+      try {
+        const lsToken = ls.get(STORAGE_AUTH_TOKEN)
+        if (lsToken) await this.login({ lsToken })
+      } catch (err) {
+        console.warn(err) // eslint-disable-line no-console
+      }
+    },
   }
 </script>
 
