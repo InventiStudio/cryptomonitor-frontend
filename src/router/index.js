4119e29a67f50aa41cb0bd88fb70f185f4a59926
Kamil Borkowski <kamil.borkowski.02@gmail.com>
2018-01-07 23:55:00 +0100

    Add routes guard for not logged-in users

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,40 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import store from 'src/store'
 
 Vue.use(Router)
 
 /* eslint-disable global-require */
 /* eslint-disable import/no-dynamic-require */
-export default new Router({
-  mode: 'history',
-  routes: [
-    {
-      path: '/',
-      name: 'Monitor',
-      component(resolve) {
-        require(['src/views/Monitor/Monitor'], resolve)
-      },
+const routes = [
+  {
+    path: '/',
+    name: 'Monitor',
+    component(resolve) {
+      require(['src/views/Monitor/Monitor'], resolve)
     },
-  ],
-})
+  },
+  {
+    path: '/sign-in',
+    name: 'SignIn',
+    component(resolve) {
+      require(['src/views/SignIn/SignIn'], resolve)
+    },
+  },
+]
 /* eslint-enable global-require */
 /* eslint-enable import/no-dynamic-require */
+
+const router = new Router({
+  mode: 'history',
+  routes,
+})
+
+router.beforeEach((to, from, next) => {
+  const { isLoggedIn } = store.state.auth
+  if (isLoggedIn && to.name === 'SignIn') return next({ name: 'Monitor' })
+  if (!isLoggedIn && to.name !== 'SignIn') return next({ name: 'SignIn' })
+  return next()
+})
+
+export default router
