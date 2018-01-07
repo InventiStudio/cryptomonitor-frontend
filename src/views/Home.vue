e23878e7685b8c5c8c9dc4b736c301594352be27
Kamil Borkowski <kamil.borkowski.02@gmail.com>
2018-01-07 17:18:18 +0100

    Change default Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,11 @@
 <template lang="pug">
-  div.home
-    img(src="~assets/logo.svg")
-    h1.c-white Hello from your awesome Vue.js project!
+  main
+    h1.c-white CryptoMonitor
 </template>
 
 <script>
-  import R from 'ramda'
-  import { mapGetters, mapActions } from 'vuex'
-
   export default {
     name: 'Home',
-    computed: {
-      ...mapGetters({
-        users: 'users/users',
-      }),
-      usersNames() {
-        return R.pluck('name')(this.users)
-      },
-    },
-    methods: {
-      ...mapActions({
-        fetchUsers: 'users/fetch',
-      }),
-    },
-    mounted() {
-      this.fetchUsers()
-    },
   }
 </script>
 
