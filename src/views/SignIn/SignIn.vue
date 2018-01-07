5683b5aa86602d24c38f6d5f635fe92430d89919
Kamil Borkowski <kamil.borkowski.02@gmail.com>
2018-01-07 23:55:50 +0100

    Add basic SignIn logic structure

---
+++ src/views/SignIn/SignIn.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+  main
+    form
+      label Email
+      input(type="email", v-model="email")
+      label Password
+      input(type="password", v-model="password")
+    button(type="button", @click="signIn") Sign in
+</template>
+
+<script>
+  import { mapActions } from 'vuex'
+  import Navbar from 'components/Navbar'
+
+  export default {
+    name: 'SignIn',
+    data() {
+      return {
+        email: '',
+        password: '',
+      }
+    },
+    components: {
+      Navbar,
+    },
+    methods: {
+      ...mapActions({
+        login: 'auth/login',
+      }),
+      async signIn() {
+        try {
+          const { email, password } = this
+          await this.login({ email, password })
+        } catch (err) {
+          throw err
+        }
+      },
+    },
+  }
+</script>
