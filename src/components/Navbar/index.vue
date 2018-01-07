8ff02279a5f6cefe04802dff058b09deb89de12c
Kamil Borkowski <kamil.borkowski.02@gmail.com>
2018-01-07 21:04:30 +0100

    Add basic Navbar

---
+++ src/components/Navbar/index.vue
@@ -0,0 +1,79 @@
+<template lang="pug">
+  header.navbar.container(:class="{ 'navbar--hidden': isNavbarHidden, 'navbar--filled': isNavbarFilled }")
+    .row.align-items-center.justify-content-between
+      .col-auto
+        router-link.d-block(:to="{ name: 'Monitor' }")
+          img(src="~assets/cryptomonitor__name.svg", alt="")
+      .col-auto
+        button.navbar__nav-button(
+          :class="{ 'navbar__nav-button--active': isNavOpen }",
+          @click="toggleNav()",
+        )
+          span Toggle nav
+        nav.navbar__nav(:class="{ 'navbar__nav--active a-fade-in': isNavOpen }")
+          h1.hidden TODO: Seo
+          ul.navbar__links
+            router-link.navbar__link(
+              :to="{ name: 'Monitor' }",
+              :exact="true",
+              active-class="navbar__link--active",
+            ) Monitor
+            router-link.navbar__cta(
+              :to="{ name: 'Monitor' }",
+              :exact="true",
+              active-class="navbar__cta--active",
+            )
+              span Sign up
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        isNavOpen: false,
+        isNavbarHidden: false,
+        isNavbarFilled: false,
+        scrollPosition: 0,
+      }
+    },
+    methods: {
+      getScrollPosition() {
+        return window.scrollY || document.documentElement.scrollTop
+      },
+      toggleNav() {
+        this.isNavOpen = !this.isNavOpen
+      },
+      toggleNavbarVisibility(scrollPosition) {
+        if (scrollPosition > this.scrollPosition && scrollPosition > 70 && !this.isNavOpen) {
+          this.isNavbarHidden = true
+          this.scrollPosition = scrollPosition
+        } else {
+          this.isNavbarHidden = false
+          this.scrollPosition = scrollPosition
+        }
+      },
+      toggleNavbarTransparency(scrollPosition) {
+        if (scrollPosition > 80) this.isNavbarFilled = true
+        else this.isNavbarFilled = false
+      },
+      handleNavbarOnScroll() {
+        const scrollPosition = this.getScrollPosition()
+        this.toggleNavbarVisibility(scrollPosition)
+        this.toggleNavbarTransparency(scrollPosition)
+      },
+    },
+    watch: {
+      '$route.path': function $routePath() {
+        this.isNavOpen = false
+      },
+    },
+    mounted() {
+      window.addEventListener('scroll', this.handleNavbarOnScroll)
+    },
+    beforeDestroy() {
+      window.removeEventListener('scroll', this.handleNavbarOnScroll)
+    },
+  }
+</script>
+
+<style src="./styles.sass" lang="sass" scoped></style>
