a5a2bb5278bc6422f7043c4b3d5cfb1f3cc1f440
Kamil Borkowski <kamil.borkowski.02@gmail.com>
2018-01-08 02:02:42 +0100

    Add form validation to SignIn

--- src/views/SignIn/index.vue
+++ src/views/SignIn/index.vue
@@ -13,34 +13,58 @@
                     We provide Vue.js front-end, Node.js back-end and UI/UX design services.
                     Whether you’re enterprise, company or startup - we can certainly help you!
                   a.o-btn.bg-black(href="//inventi.studio/en/estimate-project")
-                    span.c-white Estimate project
+                    span.fs-16.c-white Estimate project
                   a.o-btn(href="mailto:hello@inventi.studio")
-                    span.c-white or contact us!
+                    span.fs-16.c-white or contact us!
                 .col-12.col-md-6
                   h2.c-white Sign in
                   form
                     label Email
-                    input(type="email", v-model="email")
+                    input(
+                      type="email",
+                      v-model="email",
+                      placeholder="Your email address",
+                      :class="{ '': !isEmailValid }",
+                    )
+                    small
+                      span(v-show="!isEmailValid") TODO email error
                     label Password
-                    input(type="password", v-model="password")
-                  button.o-btn(type="button", @click="signIn")
-                    span.c-black Sign in
+                    input(
+                      type="password",
+                      v-model="password",
+                      placeholder="Your pass****",
+                      :class="{ '': !isPasswordValid }",
+                    )
+                    small
+                      span(v-show="!isPasswordValid") TODO password error
+                  button.o-btn(
+                    type="button",
+                    :disabled="!isFormValid",
+                    @click="signIn",
+                  )
+                    span.fs-16.c-black Sign in
 </template>
 
 <script>
+  import { required, email } from 'vuelidate/lib/validators'
   import { mapActions } from 'vuex'
   import Navbar from 'components/Navbar'
 
   export default {
     name: 'SignIn',
+    components: {
+      Navbar,
+    },
     data() {
       return {
         email: '',
         password: '',
       }
     },
-    components: {
-      Navbar,
+    computed: {
+      isEmailValid() { return !this.$v.email.$error },
+      isPasswordValid() { return !this.$v.password.$error },
+      isFormValid() { return this.isEmailValid && this.isPasswordValid },
     },
     methods: {
       ...mapActions({
@@ -48,12 +72,20 @@
       }),
       async signIn() {
         try {
-          const { email, password } = this
-          await this.login({ email, password })
+          if (this.$v.signInForm.$touch() || this.$v.signInForm.$error) return null
+          await this.login({ email: this.email, password: this.password })
         } catch (err) {
           throw err
         }
       },
+    },
+    validations: {
+      email: { required, email },
+      password: { required },
+      signInForm: [
+        'email',
+        'password',
+      ],
     },
   }
 </script>
