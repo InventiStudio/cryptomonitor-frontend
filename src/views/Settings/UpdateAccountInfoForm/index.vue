ef85096320fb2cf5e76fd92ac70c1e9e4265638c
Tomasz Krzyzowski <t.krzyzowski96@gmail.com>
2018-07-19 18:45:09 +0200

    change email address view without validation

---
+++ src/views/Settings/UpdateAccountInfoForm/index.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+  .form
+    .inline-block.mr-32(v-for="(label, i) in labels" :key="label")
+      label.c-white(:for="label.toLowerCase()") {{ label }}
+      input(:type="infoType" :id="label.toLowerCase()" :name="label.toLowerCase()" v-model="values[i]" placeholder="loolo" )
+    button.o-btn.o-btn--gray.c-black Update
+
+</template>
+
+<script>
+  export default {
+    props: {
+      infoType: String,
+      labels: Array,
+      initialValue: {
+        type: String,
+        default: '',
+      },
+      placeholders: {
+        type: Array,
+        default: ['', '', ''],
+      },
+    },
+    data() {
+      return {
+        values: this.labels.map(_ => ''),
+      }
+    },
+    created() {
+      this.values[0] = this.initialValue
+    },
+  }
+
+</script>
+
+<style lang="sass" scoped src="./styles.sass"></style>
--- src/views/Settings/index.vue
+++ src/views/Settings/index.vue
@@ -4,16 +4,22 @@
       .col-md-12
         SectionHeading(header="Personalize graph" description="Select currencies that you want to tran on monitor by default")
         CurrencyTypeForm(:currencies="currencies" :cryptoCurrencies="cryptoCurrencies")
+    .row.mt-48
+      .col-md-12
+        SectionHeading(header="Change e-mail address" description="Don't worry. We won't spam your inbox")
+        UpdateAccountInfoForm(infoType="text" :labels="emailLabels" :initialValue="accountInfo.email" :placeholders="emailPlaceholders")
 </template>
 
 <script>
   import SectionHeading from './SectionHeading'
   import CurrencyTypeForm from './CurrencyTypeForm'
+  import UpdateAccountInfoForm from './UpdateAccountInfoForm'
 
   export default {
     components: {
       SectionHeading,
       CurrencyTypeForm,
+      UpdateAccountInfoForm,
     },
     computed: {
       currencies() {
@@ -30,6 +36,29 @@
           { value: 'Bitcash', initiallyChecked: false, bgColor: 'yellow' },
           { value: 'Etherium', initiallyChecked: false, bgColor: 'violet' },
           { value: 'LittleCoin', initiallyChecked: false, bgColor: 'transparent' },
+        ]
+      },
+      emailLabels() {
+        return [
+          'Old email address',
+          'New e-mail address',
+          'Repeat new e-mail address',
+        ]
+      },
+      emailPlaceholders() {
+        return ['some@email.com','some@email.com','some@email.com']
+      },
+      accountInfo() {
+        return {
+          email: 'john.doe@mail.com',
+          password: 'password',
+        }
+      },
+      passwordLabels() {
+        return [
+          'Old password',
+          'New password',
+          'Repeat password',
         ]
       },
     },
